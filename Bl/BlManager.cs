7aa9fa8f42ae825c18e7ab8c5b8af701e3089c9a
LevMiriam <0666miri@gmail.com>
2025-05-12 17:40:53 +0300

    Finish customers services

---
+++ Bl/BlManager.cs
@@ -0,0 +1,23 @@
+﻿using Bl.Api;
+using Bl.Services;
+using Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl
+{
+    public class BlManager: IBlManager
+    {
+        public IBlCustomers BlCustomers { get; }
+        public IBlOrders BlOrder { get; }
+        
+        public BlManager(IDalManager dalManager)
+        {
+			BlCustomers = new BlCustomersService(dalManager.DalCustomers);
+			BlOrder = new BlOrderService(dalManager.DalOrders);
+        }
+    }
+}

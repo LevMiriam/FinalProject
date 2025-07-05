bdeeb70b260b1ce288c43f0d73951c910c48763b
MenuchiFeldman <m0548488576@gmail.com>
2025-07-06 02:31:43 +0300

    I saved changes before merging.

--- Bl/obj/Debug/net8.0/Bl.AssemblyInfo.cs
+++ Bl/obj/Debug/net8.0/Bl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe89f52b337528700f409b72eda52919d558ef4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddfa4091d8e102e5bb559f8c1e72e36dff0f20dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
